8e3c0c92d4a588d71826420c4b03e120cff80a2e
fruitpunchsamurai7 <33376298+fruitpunchsamurai7@users.noreply.github.com>
2019-07-02 21:36:10 -0400

    Update rpsls_assignment.rb

--- rpsls_assignment.rb
+++ rpsls_assignment.rb
@@ -1,10 +1,12 @@
 p_score = 0
 c_score = 0
 choice = ''
+answer = ''
 
 NEW_LINE = "\n".freeze
-
 CHOICES = %w(rock paper scissors lizard spock).freeze
+VALID_ANSWER = %w(yes y no n).freeze
+ROUNDS = 5
 
 CHOICES_SHORTCUT = {
   'r' => ['rock'],
@@ -34,6 +36,11 @@ def win?(first, second)
   WINNING_CHOICES[first].include?(second)
 end
 
+def display_initial_message
+  prompt 'Welcome to the RPSLS Game'
+  prompt 'Score ROUNDS wins to be the Grand Winner!' + (NEW_LINE * 2)
+end
+
 def display_result(player, computer)
   if win?(player, computer)
     prompt 'You won!'
@@ -46,22 +53,30 @@ end
 
 def score_prompt(p_score, c_score)
   prompt "Your score is #{p_score}. Computer score is #{c_score}"
-  prompt 'You are the grand winner!' if p_score == 5
-  prompt 'Computer is the grand winner!' if c_score == 5
+  prompt 'You are the grand winner!' if p_score == ROUNDS
+  prompt 'Computer is the grand winner!' if c_score == ROUNDS
 end
 
-prompt 'Welcome to the RPSLS Game'
-prompt 'Score 5 wins to be the Grand Winner!' + (NEW_LINE * 2)
-loop do
+def retrieve_move_input(input)
   loop do
     prompt "Choose either #{CHOICES.join(', ')}"
     prompt "( You can also use shortcuts- #{CHOICES_SHORTCUT.keys.join(', ')})"
-    choice = gets.chomp
+    input = gets.downcase.chomp
 
-    break if CHOICES.include?(choice)
-    break if CHOICES_SHORTCUT.keys.include?(choice)
+    break if CHOICES.include?(input)
+    break if CHOICES_SHORTCUT.keys.include?(input)
     prompt "That's not a valid choice."
   end
+  input
+end
+
+def match_ended?(p_score, c_score)
+  (p_score == ROUNDS) || (c_score == ROUNDS)
+end
+
+display_initial_message
+loop do
+  choice = retrieve_move_input(choice)
 
   choice = shortcut(choice).join('') if CHOICES_SHORTCUT.keys.include?(choice)
 
@@ -77,13 +92,18 @@ loop do
   score_prompt(p_score, c_score)
   prompt NEW_LINE
 
-  if (p_score == 5) || (c_score == 5)
+  if match_ended?(p_score, c_score)
     prompt 'Do you want to play again? (Y for yes, all else no).'
-    answer = gets.chomp
+    loop do
+      answer = gets.downcase.chomp
+      break if VALID_ANSWER.include?(answer)
+      puts 'Invalid answer. Enter Yes,No,Y,N'
+    end
     break unless answer.downcase.start_with?('y')
+    system('clear') || system('cls')
   end
 
-  (p_score = 0) && (c_score = 0) if (p_score == 5) || (c_score == 5)
+  (p_score = 0) && (c_score = 0) if match_ended?(p_score, c_score)
 end
 
 prompt 'Thank you for playing, goodbye!'
